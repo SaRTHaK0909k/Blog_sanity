97c47c1720d81fdf0e1de9d0728f999c7888c650
sarthak ganure <sarthakganure@sarthaks-MacBook-Air.local>
2023-10-25 22:49:20 +0530

    connected and linked pages dynamically

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,31 +1,82 @@
-import React from 'react'
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
-import { sanityClient, urlFor } from '../../sanity';
-import { Post } from '../../typings';
-import { getStaticProps } from 'next';
+import React from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import { sanityClient, urlFor } from "../../sanity";
+import { Post } from "../../typings";
+import PortableText from "react-portable-text"
 
 
 
-interface Props{
-    post:Post
+interface Props {
+  post: Post;
 }
-const Post = ({ post }:Props) => {
+const Post = ({ post }: Props) => {
   return (
     <div>
-      <Header/>
-      <img src={urlFor(post.mainImage.url()!)} alt="coverImage" />
-      <div className="py-20">
-        {/* Render your post content here */}
+      <Header />
+      <img
+        className="w-full h-96 object-cover"
+        src={urlFor(post.mainImage).url()!}
+        alt="coverImage"
+      />
+      <div className="max-w-3xl mx-auto">
+        <article className="w-full mx-auto p-5 bg-secondaryColor/10">
+          <h1 className="font-titleFont font-medium text-[32px] text-primary border-b-[1px] border-b-cyan-800 mt-10 mb-3">
+            {post.title}
+          </h1>
+          <h2 className="font-bodyFont text-[18px] text-gray-500 mb-2">
+            {post.description}
+          </h2>
+          <div className="flex items-center gap-2">
+            <img
+              className="rounded-full w-12 h-12 object-cover bg-red-400"
+              src={urlFor(post.author.image).url()}
+              alt="authorImg"
+            />
+            <p className="font-bodyFont text-base">
+              Blog post by <span className="font-bold text-secondaryColor">{post.author.name}</span> - Published at{" "}
+              {new Date(post.publishedAt).toLocaleDateString()}{" "}
+            </p>
+          </div>
+          <div className="mt-10">
+            <PortableText dataset={"production"}
+                projectId={"rofbwy93"}
+                content={post.body}
+                serializers={{
+                  h1:(props:any)=>(
+                      <h1 className="text-3xl font-bold my-5 font-titleFont"
+                      {...props}/>
+                  ),
+                  h2:(props:any)=>(
+                    <h2 className="text-2xl font-bold my-5 font-titleFont"
+                    {...props}/>
+                  ),
+                  h3:(props:any)=>(
+                    <h3 className="text-2xl font-bold my-5 font-titleFont"
+                    {...props}/>
+                  ),
+                  li:({children}:any)=>(
+                    <li className="ml-4 list-disc">{children}</li>
+                  ),
+                  link:({href,children}:any)=>(
+                    <a href={href} className="text-cyan-500 hover:underline">
+                      {children}
+                    </a>
+                  ),
+
+                }}
+            />
+          </div>
+        </article>
       </div>
-      <Footer/>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default Post;
 
-export const getPostStaticPaths = async () => {
+export const getStaticPaths = async () => {
   const query = `*[_type == "post"]{
       _id,
       slug{
@@ -34,45 +85,46 @@ export const getPostStaticPaths = async () => {
   }`;
   const posts = await sanityClient.fetch(query);
   const paths = posts.map((post: Post) => ({
-    slug: post.slug.current,
+    params: {
+      slug: post.slug.current,
+    },
   }));
   return {
     paths,
     fallback: "blocking",
   };
-}
+};
 
-export const getPostStaticProps = async ({ params }) => {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const query = `*[_type == "post" && slug.current == $slug][0]{
-      _id,
-      publishedAt,
-      title,
-      author -> {
-          name,
-          image,
-      },
-      description,
-      mainImage,
-      slug,
-      body
-  }` 
+    _id,
+     publishedAt,
+     title,
+     author ->{
+      name,
+      image,
+     },
+     description,
+     mainImage,
+     slug,
+     body
+  }`;
 
-  const post = await sanityClient.fetch(query,{
-      slug: params?.slug,
-  })
-  if(!post){
-      return {
-          notFound:true
-      }
+  const post = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  });
+  if (!post) {
+    return {
+      notFound: true,
+    };
   }
   return {
-      props:{
-          post
-      },
-      revalidate:60,
-  }
-}
-
+    props: {
+      post,
+    },
+    revalidate: 60,
+  };
+};
 
 // import React from 'react'
 // import Header from '../../components/Header';
@@ -93,7 +145,6 @@ export const getPostStaticProps = async ({ params }) => {
 // }
 
 // export default Post
-
 
 // export const getStaticPaths = async()=>{
 //     const query = `*[_type == "post"]{
@@ -125,7 +176,7 @@ export const getPostStaticProps = async ({ params }) => {
 //         mainImage,
 //         slug,
 //         body
-//     }` 
+//     }`
 
 //     const post = await sanityClient.fetch(query,{
 //         slug:params?.slug,
